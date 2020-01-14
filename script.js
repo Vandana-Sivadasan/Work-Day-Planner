349a7259a61f03500ea3509820bfe9d4b40d1723
Vandana Sivadasan <sivadasanvandana62@gmail.com>
2020-01-13 21:44:26 -0600

    add files

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    console.log("document is ready")
+  });
+  function submitform(){
+
+     var numbers = document.getElementById("numbers")
+     console.log("numbers" + numbers)
+  }
